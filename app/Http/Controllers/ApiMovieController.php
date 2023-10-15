3396effcfd049d51604b5bd4897c8a7ea4594ced
huonghoang9 <huohoang9@gmail.com>
2023-10-15 08:14:06 +0700

    crud_movie

---
+++ app/Http/Controllers/ApiMovieController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\MovieResource;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class ApiMovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // lấy ra toàn bộ danh danh sách
+        $movie = Movie::all();
+//        Trả về danh sách dưới dạng json
+        return MovieResource::collection($movie);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $movie = Movie::create($request->all());
+//        trả về thông vừa thêm
+        return new MovieResource($movie);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+//    Hiển thị sửa
+    public function show(string $id)
+    {
+        //
+        $movie = Movie::find($id);
+        if($movie){
+            return new MovieResource($movie);
+        }else{
+            return  response()->json(['message'=>'Không tồn tại'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $movie = Movie::find($id);
+        if($movie){
+            $movie->update($request->all());
+        }else{
+            return  response()->json(['message'=>'Không tồn tại'], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $movie = Movie::find($id);
+        if($movie){
+            $movie->delete();
+            return  response()->json(['message'=>'Xóa thành công'], 280);
+        }else{
+            return  response()->json(['message'=>'Không tồn tại'], 404);
+        }
+    }
+}
