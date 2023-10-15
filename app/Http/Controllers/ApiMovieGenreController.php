edee696039237678a7a07280f90411cf67f945a2
huonghoang9 <huohoang9@gmail.com>
2023-10-15 08:14:31 +0700

    crud_movieGenre

---
+++ app/Http/Controllers/ApiMovieGenreController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\MovieGenreResource;
+use App\Models\Movie_Genre;
+use Illuminate\Http\Request;
+
+class ApiMovieGenreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // lấy ra toàn bộ danh danh sách
+        $movieGenre = Movie_Genre::all();
+//        Trả về danh sách dưới dạng json
+        return MovieGenreResource::collection($movieGenre);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $movieGenre = Movie_Genre::create($request->all());
+//        trả về thông vừa thêm
+        return new MovieGenreResource($movieGenre);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+//    Hiển thị sửa
+    public function show(string $id)
+    {
+        //
+        $movieGenre = Movie_Genre::find($id);
+        if($movieGenre){
+            return new MovieGenreResource($movieGenre);
+        }else{
+            return  response()->json(['message'=>'Không tồn tại'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $movieGenre = Movie_Genre::find($id);
+        if($movieGenre){
+            $movieGenre->update($request->all());
+        }else{
+            return  response()->json(['message'=>'Không tồn tại'], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $movieGenre = Movie_Genre::find($id);
+        if($movieGenre){
+            $movieGenre->delete();
+            return  response()->json(['message'=>'Xóa thành công'], 280);
+        }else{
+            return  response()->json(['message'=>'Không tồn tại'], 404);
+        }
+    }
+}
