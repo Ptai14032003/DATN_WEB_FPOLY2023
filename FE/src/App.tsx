42c9dee4cddff2911288c9480e9ec02839f5937a
Quynhntdph25045 <quynhntdph25045@fpt.edu.vn>
2023-11-11 09:02:48 +0700

    layout login,register

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,17 +1,41 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LayoutAdmin from './components/layouts/layoutAdmin/page'
-import AdminQlSc from './admin/qlSc/page'
+import AdminQlSc from './admin/qlsc/page'
 import AdminQlSp from './admin/qlSp/page'
 import AdminQlNhanSu from './admin/qlNhanSu/page'
 import AdminQlGuest from './admin/qlGuest/page'
 import AdminQlDiscount from './admin/discount/page'
 import AdminQlPhongChieu from './admin/qlphongChieu/page'
 import AdminQlPhim from './admin/qlPhim/page'
+import LayoutPerson from './components/layouts/layoutGuest/LayoutPerson'
+import NewFilm from './personPage/newFilm'
+import HomePage from './personPage/Homepage'
+import Signin from './personPage/Signin'
+import Signup from './personPage/Signup'
+import Booking from './personPage/Booking'
+import SeatBooking from './personPage/SeatBooking'
+import Detail from './personPage/detail'
 function App() {
+
+
   return <BrowserRouter>
     <Routes>
-      <Route path='/'>
+      <Route path='/' element={<LayoutPerson />}>
+        <Route index element={<HomePage  />} />
+        <Route path="new" element={<NewFilm />} />
       </Route>
+      <Route path="signin" element={<Signin />} />
+      <Route path="signup" element={<Signup />} />
+
+      <Route path='movies/:id' element={<Detail />}></Route>
+      <Route path='booking/:id' element={<Booking />}>
+        <Route index element={<SeatBooking />} />
+        {/* <Route path="combo" element={<ComboBooking/>} /> */}
+      </Route>
+
+
+
+
       <Route path='/admin' element={<LayoutAdmin />}>
         <Route path='qlPhim' element={<AdminQlPhim />}></Route>
         <Route path='qlSuatChieu' element={<AdminQlSc />}></Route>
