e1a422c44f5e9e419f676b0e71768e33583ca0b4
huonghoang9 <huohoang9@gmail.com>
2023-11-24 14:32:26 +0700

    update_api_movies

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\RoomApiController;
-use App\Http\Controllers\Api\ShowtimeApiController;
+use App\Http\Controllers\RoomApiController;
+use App\Http\Controllers\ShowtimeApiController;
 use App\Http\Controllers\ApiActorController;
 use App\Http\Controllers\ApiCountryController;
 use App\Http\Controllers\ApiListGenreController;
