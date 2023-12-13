200d436b382328d3fbec50d1d8ef9dc77453f44a
Ptai14032003 <ptai14032003@gmail.com>
2023-12-13 14:16:48 +0700

    13/12

--- FE/src/components/itemGuest/ThanhToan/ThanhToan.tsx
+++ FE/src/components/itemGuest/ThanhToan/ThanhToan.tsx
@@ -50,7 +50,7 @@ const ThanhToan: React.FC<Props> = ({ data: { selectedSeats, priceTong, combo, s
         ,
         total_money: priceTong
     }
-    const setThanhToan = () => {
+        const setThanhToan = () => {
         data(dataBill)
             .then((response) => {
                 if (('data' in response)) {
