6657eecaf4ad39aa8f086b3b6f5ea667d995b289
Ptai14032003 <ptai14032003@gmail.com>
2024-01-09 16:58:33 +0700

    09/01 +

--- BE/app/Http/Controllers/PersonnelController.php
+++ BE/app/Http/Controllers/PersonnelController.php
@@ -41,7 +41,7 @@ class PersonnelController extends Controller
                     'required',
                     'regex:/^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/',
                     'unique:personnels,phone_number',
-                    'unique:users,phone_number'
+                    'unique:users,phone_number',
                 ],
                 'password' => [
                     'required',
