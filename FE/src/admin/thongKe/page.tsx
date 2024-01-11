cd7e0c501cb1a3f5406763ebfa64edfef11bd4e4
dinhqtai <dinhquoctai2003@gmail.com>
2024-01-11 16:53:44 +0700

    Update page.tsx

--- FE/src/admin/thongKe/page.tsx
+++ FE/src/admin/thongKe/page.tsx
@@ -1,9 +1,12 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+import type { DatePickerProps, TimePickerProps } from 'antd';
 import { useRevenueAllAPIMutation } from '../../rtk/statistics/statistics';
-import { Space, DatePicker, DatePickerProps, Table } from 'antd';
+import { Space, DatePicker, DatePickerProps, Select, Button } from 'antd';
 import { useEffect, useState } from 'react';
-import Column from 'antd/es/table/Column';
 const { RangePicker } = DatePicker;
+const { Option } = Select;
 interface TotalRevenue {
     quantity_bill: number,
     total_money: number,
@@ -18,25 +21,54 @@ const ThongKe = () => {
     const [total_money, setData_money] = useState<any>("0")
     const [total_money_food, setData_food] = useState<any>("0")
     const [total_money_ticket, setData_ticket] = useState<any>("0")
-    const [dataTable, setDataTable] = useState<any>()
-    const onChange: DatePickerProps['onChange'] = (date) => {
+    const [type, setType] = useState<any>('year');
+    const [valueDate, setvalueDate] = useState<any>((new Date)?.getFullYear())
+    const [dataChart, setDataChart] = useState<any>()
+    const [traCuu, setTraCuu] = useState<any>()
+    const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+        setvalueDate(dateString)
         if (date) {
             const dateData = {
-                timeline: "momth",
+                timeline: type,
                 year: date?.year(),
                 month: Number(date?.month() + 1),
             }
-            getRevenueAll(dateData).then((data) => {
-                setData(data)
-            })
+            setTraCuu(dateData)
         }
     };
+    const handleSelect = () => {
+        if (traCuu) {
+            getRevenueAll(traCuu).then((fetchdata: any) => {
+                if (fetchdata?.data?.error) {
+                    setData({
+                        quantity_bill: 0,
+                        total_money: 0,
+                        total_money_ticket: 0,
+                        total_money_food: 0
+                    })
+                    if (type === "month") {
+                        setDataChart(fetchdata?.data?.dailyRevenue)
+                    } else {
+                        setDataChart(fetchdata?.data?.monthlyRevenue)
+                    }
+                } else {
+                    setData(fetchdata?.data)
+                    if (type === "month") {
+                        setDataChart(fetchdata?.data?.dailyRevenue)
+                    } else {
+                        setDataChart(fetchdata?.data?.monthlyRevenue)
+                    }
+                }
+
+            })
+        }
+    }
     useEffect(() => {
         const currentTime = new Date()
         if (data) {
-            const total_money = (Number(data?.data?.total_money))?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            const total_money_ticket = (Number(data?.data?.total_money_ticket))?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            const total_money_food = (Number(data?.data?.total_money_food))?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            const total_money = (Number(data?.total_money))?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") || 0
+            const total_money_ticket = (Number(data?.total_money_ticket))?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") || 0
+            const total_money_food = (Number(data?.total_money_food))?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") || 0
             setData_money(total_money);
             setData_food(total_money_food);
             setData_ticket(total_money_ticket);
@@ -47,36 +79,87 @@ const ThongKe = () => {
                     year: currentTime?.getFullYear(),
                     month: currentTime?.getMonth() + 1,
                 }
-                console.log(dateData);
-
-                getRevenueAll(dateData).then((data) => {
-                    setData(data)
+                getRevenueAll(dateData).then((fetchdata: any) => {
+                    if (fetchdata?.data?.error) {
+                        setData({
+                            quantity_bill: 0,
+                            total_money: 0,
+                            total_money_ticket: 0,
+                            total_money_food: 0
+                        })
+                        if (type === "month") {
+                            setDataChart(fetchdata?.data?.dailyRevenue)
+                        } else {
+                            setDataChart(fetchdata?.data?.monthlyRevenue)
+                        }
+                    } else {
+                        setData(fetchdata?.data)
+                        if (type === "month") {
+                            setDataChart(fetchdata?.data?.dailyRevenue)
+                        } else {
+                            setDataChart(fetchdata?.data?.monthlyRevenue)
+                        }
+                    }
                 })
             }
     }, [data])
-    console.log(data);
-
     return (
-        <div>
-            <Space direction="vertical" size={12}>
-                <DatePicker picker="month" onChange={onChange} format={`MM / YYYY`} />
-            </Space>
-            <div>
-                <div>Doanh thu tháng này</div>
+        <>
+            <div className="flex gap-20 mt-[5%]">
                 <div>
-                    <div>Tổng doanh thu :</div>
-                    <div>{total_money}</div>
+                    <Space direction="vertical" size={12}>
+                        <div className="flex gap-[40px]">
+                            <Select value={type} onChange={setType}>
+                                <Option value="month">Month</Option>
+                                <Option value="year">Year</Option>
+                            </Select>
+                            <DatePicker picker={type} onChange={onChange} format={`${type === 'month' ? "MM / YYYY" : "YYYY"}`} />
+                            <Button onClick={() => handleSelect()}>Tra cứu</Button>
+                        </div>
+                    </Space>
+                    <div className="py-5">
+                        <div className="text-2xl font-bold">Doanh thu</div>
+                        <div className="py-5">
+                            <div>Tổng doanh thu :</div>
+                            <div>{total_money}</div>
+                        </div>
+                        <div className="">
+                            <div>Doanh thu phim :</div>
+                            <div>{total_money_ticket}</div>
+                        </div>
+                        <div className="py-5">
+                            <div>Doanh thu sản phẩm :</div>
+                            <div>{total_money_food}</div>
+                        </div>
+                        <div>
+                            <div>Chu kì : {type === "month" ? "Theo tháng" : "Theo năm"}</div>
+                            <div className="py-2">Thời gian : {valueDate}</div>
+                            <div>Số hoá đơn : {data?.quantity_bill}</div>
+                        </div>
+                    </div>
                 </div>
                 <div>
-                    <div>Doanh thu phim :</div>
-                    <div>{total_money_ticket}</div>
-                </div>
-                <div>
-                    <div>Doanh thu sản phẩm :</div>
-                    <div>{total_money_food}</div>
+                    <LineChart
+                        width={800}
+                        height={350}
+                        data={dataChart}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey={`${type === "month" ? "date" : "month"}`} />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="total_money" stroke="#82ca9d" />
+                    </LineChart>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
