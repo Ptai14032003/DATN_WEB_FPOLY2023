9fa673f3d6096b50dfe80d63cd3ede42b7b4dc25
Ptai14032003 <ptai14032003@gmail.com>
2024-01-17 16:44:09 +0700

    17/1

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -32,8 +32,8 @@ import ExportTicket from './admin/export/exportTicket.tsx'
 import ShowingFilm from './personPage/showingFilm.tsx'
 import ComingSoonFilm from './personPage/comingSoonFilm.tsx'
 import ForgotPassword from './personPage/ForgotPassword.tsx'
-import AdminQlSc from './admin/qlSc/page.tsx'
 import PrivacyPolicy from './personPage/privacyPolicy.tsx'
+import AdminQlSc from './admin/qlSc/page.tsx'
 
 function App() {
   const checkLocal = localStorage.getItem("user");
@@ -67,7 +67,6 @@ function App() {
       <Route path="profile" element={<Profile />} />
       <Route path="forgot-password" element={<ForgotPassword />} />
       <Route path="reset_password?" element={<ResetPassword />} />
-
       <Route path="signin" element={<Signin />} />
       <Route path="signup" element={<Signup />} />
       <Route path='movie_show_time/:id' element={<Detail />}></Route>
